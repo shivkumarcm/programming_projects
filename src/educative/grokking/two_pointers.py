faaeee1953b798cf95a89dd5da3997ef427486b7
shivkumarcm <164088249+shivkumarcm@users.noreply.github.com>
2024-03-21 22:04:21 -0400

    Update two_pointers.py
    This is the package with all functions that match this pattern

--- src/educative/grokking/two_pointers.py
+++ src/educative/grokking/two_pointers.py
@@ -1,5 +1,28 @@
+def is_palindrome(s):
+  """
+  Given a string, s, as an input and determines whether or
+  not it is a palindrome
+  """
+  if s == None:
+    return False
+  
+  start = 0
+  end = len(s) - 1
+  
+  while start < end:
+    if s[start] != s[end]:
+      return False
+    start = start + 1
+    end = end - 1
+  return True
+
 
 def find_sum_of_three(nums, target):
+   """
+   Given an array of integers, nums, and an integer value, target,
+   determine if there are any three integers in nums whose sum is
+   equal to the target.
+   """
    if len(nums) < 3:
       return False
    nums.sort()
