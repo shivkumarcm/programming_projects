44a6b369d574fc156647b3f01b74ab362f74e2d9
shivkumarcm <164088249+shivkumarcm@users.noreply.github.com>
2024-03-21 22:35:22 -0400

    Update linked_list_node.py
    moving to utils

--- src/educative/grokking/utils/linked_list_node.py
+++ src/educative/grokking/utils/linked_list_node.py
@@ -1,1 +1,8 @@
+# Source: https://www.educative.io/courses/grokking-coding-interview-patterns-python/remove-nth-node-from-end-of-list
+# Template for linked list node class
 
+class LinkedListNode:
+    # __init__ will be used to make a LinkedListNode type object.
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
