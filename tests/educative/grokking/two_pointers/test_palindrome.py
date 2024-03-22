67a8eb2dcfaf74de327823eb864e4aa3364f1a38
shivkumarcm <164088249+shivkumarcm@users.noreply.github.com>
2024-03-22 10:51:23 -0400

    Update test_palindrome.py

--- tests/educative/grokking/two_pointers/test_palindrome.py
+++ tests/educative/grokking/two_pointers/test_palindrome.py
@@ -3,12 +3,12 @@ from educative.grokking.two_pointers import is_palindrome
 
 class TestPalindrome(unittest.TestCase):
 
-  def test_cases():
-    assertTrue(is_palindrome("kayak"))
-    assertFalse(is_palindrome("hello"))
-    assertFalse(is_palindrome("RACEACAR"))
-    assertTrue(is_palindrome("A"))
-    assertFalse(is_palindrome("ABCDABCD"))
+  def test_cases(self):
+    self.assertTrue(is_palindrome("kayak"))
+    self.assertFalse(is_palindrome("hello"))
+    self.assertFalse(is_palindrome("RACEACAR"))
+    self.assertTrue(is_palindrome("A"))
+    self.assertFalse(is_palindrome("ABCDABCD"))
 
 if __name__ == '__main__':
     unittest.main()
