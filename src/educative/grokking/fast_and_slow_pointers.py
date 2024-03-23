5a5a05615bd963034d9766dc1bc37b9653dc7a0d
shivkumarcm <shivkumarcm@users.noreply.github.com>
2024-03-22 23:53:32 -0400

    list palindrome code and tests

--- src/educative/grokking/fast_and_slow_pointers.py
+++ src/educative/grokking/fast_and_slow_pointers.py
@@ -53,3 +53,61 @@ def get_middle_node(head):
     fast_ptr = fast_ptr.next
     if fast_ptr:
       fast_ptr = fast_ptr.next
+
+def find_duplicate(nums):
+  """
+  See https://www.educative.io/courses/grokking-coding-interview-patterns-python/find-the-duplicate-number
+
+  """
+  slow_ptr = 0
+  fast_ptr = 0
+  while True:
+    slow_ptr = nums[slow_ptr]
+    fast_ptr = nums[nums[fast_ptr]]
+    if slow_ptr == fast_ptr:
+      break
+
+  slow_ptr = 0
+  while slow_ptr != fast_ptr:
+    slow_ptr = nums[slow_ptr]
+    fast_ptr = nums[fast_ptr]
+
+  return slow_ptr
+
+def rev_list(head):
+  prev = None
+  ptr = head
+  while ptr:
+    rest = ptr.next
+    ptr.next = prev
+    prev = ptr
+    ptr = rest
+  return prev
+
+
+def comp_list(h1, h2):
+  while h1 and h2:
+    if h1.data == h2.data:
+      h1 = h1.next
+      h2 = h2.next
+    else:
+      return False
+  return True
+
+
+def palindrome(head):
+  if not head:
+    return False
+
+  slow_ptr = head
+  fast_ptr = head
+
+  while fast_ptr and fast_ptr.next:
+    slow_ptr = slow_ptr.next
+    fast_ptr = fast_ptr.next.next
+
+  second_half = rev_list(slow_ptr)
+  retval = comp_list(head, second_half)
+  rev_list(second_half)
+
+  return retval
